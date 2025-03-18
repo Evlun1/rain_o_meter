c6d9a1e5c9d22d2dec920d491d804d11188d30c4
Evlun1 <45795406+Evlun1@users.noreply.github.com>
2025-03-18 16:45:07 +0100

    backend: add basic repo structure

---
+++ backend/tests/test_main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
+
+
+def test_read_item():
+    input_id = "99"
+    response = client.get(f"/items/{input_id}")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": int(input_id), "q": None}
